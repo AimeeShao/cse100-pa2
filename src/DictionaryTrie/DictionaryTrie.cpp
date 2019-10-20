85efacd371b962a275e2c8abd32ae4b6524174ba
Aimee Shao <aimeeshao101@gmail.com>
2019-10-20 21:32:32 +0000

    Initial headers and hpp file

--- src/DictionaryTrie/DictionaryTrie.cpp
+++ src/DictionaryTrie/DictionaryTrie.cpp
@@ -1,18 +1,30 @@
 /**
- * TODO: File Header
+ * A dictionary trie used for storing strings and words. This allows for
+ * inserting and finding words. Implements a ternary search trie.
  *
- * Author:
+ * Author: Aimee T Shao
+ * Email: atshao@ucsd.edu
  */
 #include "DictionaryTrie.hpp"
 #include <iostream>
 
-/* TODO */
-DictionaryTrie::DictionaryTrie() {}
+/* Constructor.
+ * Initializes the dictionary trie.
+ */
+DictionaryTrie::DictionaryTrie() { root = nullptr; }
 
-/* TODO */
+/* Inserts a word into the dictionary trie with an assigned frequency.
+ * Creates TrieNodes to store letters in the word along with way.
+ * @param word Word to insert into the dictionary trie
+ * @param freq Frequency of the word
+ * @return True if we successfully inserted. Otherwise, false.
+ */
 bool DictionaryTrie::insert(string word, unsigned int freq) { return false; }
 
-/* TODO */
+/* Finds a query word in the dictionary trie.
+ * @param word Query word to find in trie
+ * @return True if we found the word. False otherwise.
+ */
 bool DictionaryTrie::find(string word) const { return false; }
 
 /* TODO */
@@ -27,5 +39,5 @@ std::vector<string> DictionaryTrie::predictUnderscores(
     return {};
 }
 
-/* TODO */
+/* Deallocates the dictionary trie. */
 DictionaryTrie::~DictionaryTrie() {}
--- src/DictionaryTrie/DictionaryTrie.hpp
+++ src/DictionaryTrie/DictionaryTrie.hpp
@@ -1,7 +1,10 @@
 /**
- * TODO: File Header
+ * The header of the dictionary trie used for storing strings and words. The
+ * trie allows for inserting and finding words.
+ * Implements a ternary search trie.
  *
- * Author:
+ * Author: Aimee T Shao
+ * Email: atshao@ucsd.edu
  */
 #ifndef DICTIONARY_TRIE_HPP
 #define DICTIONARY_TRIE_HPP
@@ -17,16 +20,47 @@ using namespace std;
  * a mulit-way trie or a ternary search tree.
  */
 class DictionaryTrie {
+    /* The class for a trie node that will store a letter to help build up the
+     * ternary search tree.
+     */
+    class TrieNode {
+      public:
+        TrieNode* left;
+        TrieNode* right;
+        char const data;  // the constant data in this node
+        bool const word;  // determines if this is a word node
+        int const freq;   // frequency of this word if word node
+
+        /* Constructor.
+         * Initializes a TrieNode with given data.
+         * @param c Data/element of this node
+         */
+        TrieNode(const char& d) : data(d), word(false), freq(0) {
+            left = right = nullptr;
+        }
+    };
+
   private:
-    // TODO: add private members and helper methods here
+    TrieNode* root;  // stores the root of the dictionary trie
+
   public:
-    /* TODO: add function header */
+    /* Constructor.
+     * Initializes the dictionary trie.
+     */
     DictionaryTrie();
 
-    /* TODO: add function header */
+    /* Inserts a word into the dictionary trie with an assigned frequency.
+     * Creates TrieNodes to store letters in the word along with way.
+     * @param word Word to insert into the dictionary trie
+     * @param freq Frequency of the word
+     * @return True if we successfully inserted. Otherwise, false.
+     */
     bool insert(string word, unsigned int freq);
 
-    /* TODO: add function header */
+    /* Finds a query word in the dictionary trie.
+     * @param word Query word to find in trie
+     * @return True if we found the word. False otherwise.
+     */
     bool find(string word) const;
 
     /* TODO: add function header */
@@ -37,7 +71,7 @@ class DictionaryTrie {
     vector<string> predictUnderscores(string pattern,
                                       unsigned int numCompletions);
 
-    /* TODO: add function header */
+    /* Deallocates the dictionary trie. */
     ~DictionaryTrie();
 };
 
