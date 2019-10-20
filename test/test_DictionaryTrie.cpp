90698990a75f76e99de2975ae7992a16212644ff
Aimee Shao <aimeeshao101@gmail.com>
2019-10-20 21:55:10 +0000

    Wrote initial tests for dictionary trie

--- test/test_DictionaryTrie.cpp
+++ test/test_DictionaryTrie.cpp
@@ -24,4 +24,53 @@ TEST(DictTrieTests, EMPTY_TEST) {
     ASSERT_EQ(dict.find("abrakadabra"), false);
 }
 
-/* TODO */
+/* TrieNode test */
+TEST(DictTrieTests, TRIE_NODE_TEST) {
+    DictionaryTrie::TrieNode node('w');
+    ASSERT_EQ(node.data, 'w');
+}
+
+/* Small Insert and find Test */
+TEST(DictTrieTests, SMALL_TEST) {
+    DictionaryTrie dict;
+    dict.insert("word", 10);
+    ASSERT_TRUE(dict.find("word"));
+}
+
+/* Large Insert and find Test */
+TEST(DictTrieTests, LARGE_TEST) {
+    DictionaryTrie dict;
+    dict.insert("call", 5);
+    dict.insert("me", 20);
+    dict.insert("mind", 2);
+    dict.insert("mid", 10);
+    // Assert that we can find mind
+    ASSERT_TRUE(dict.find("mind"));
+}
+
+/* Large find false Test */
+TEST(DictTrieTests, LARGE_FIND_FALSE_TEST) {
+    DictionaryTrie dict;
+    dict.insert("call", 5);
+    dict.insert("me", 20);
+    dict.insert("mind", 2);
+    dict.insert("mid", 10);
+    // Assert that we cannot find middle
+    ASSERT_FALSE(dict.find("middle"));
+}
+
+/* Predict Completions test */
+TEST(DictTrieTests, PREDICT_COMPLETIONS_FTEST) {
+    DictionaryTrie dict;
+    dict.insert("call", 5);
+    dict.insert("me", 20);
+    dict.insert("mind", 2);
+    dict.insert("mid", 10);
+
+    vector<string> answer;
+    answer.emplace_back("me");
+    answer.emplace_back("mid");
+
+    // Assert that predict completions works correctly
+    ASSERT_EQ(dict.predictCompletions("m", 2), answer);
+}
